3e01dbdfb10ed35f689e7d876790f48479b2bd76
Yaasir Ketwaroo <608793+ketwaroo@users.noreply.github.com>
2023-12-27 20:59:43 -0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,120 @@
+-- Unsolicited Crafting recipes.
+-- 2023 (c) Ketwaroo
+
+minetest.register_on_mods_loaded(function()
+    if minetest.get_modpath("mcl_mobitems") then
+        if minetest.settings:get_bool("mcl_misk_recipes.enable_horse_stuff", true) then
+            -- minetest.register_craft({
+            -- 	output = "mcl_mobitems:leather_horse_armor",
+            -- 	recipe = {
+            -- 		{ "mcl_mobitems:leather", "",                     "mcl_mobitems:leather" },
+            -- 		{ "mcl_mobitems:leather", "mcl_mobitems:leather", "mcl_mobitems:leather" },
+            -- 		{ "mcl_mobitems:leather", "",                     "mcl_mobitems:leather" },
+            -- 	}
+            -- })
+
+            minetest.register_craft({
+                output = "mcl_mobitems:saddle",
+                recipe = {
+                    { "mcl_mobitems:leather", "mcl_mobitems:leather", "mcl_mobitems:leather" },
+                    { "mcl_core:iron_nugget", "",                     "mcl_core:iron_nugget" },
+                }
+            })
+
+            minetest.register_craft({
+                output = "mcl_mobitems:iron_horse_armor",
+                recipe = {
+                    { "mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget" },
+                    { "mcl_core:iron_nugget", "mcl_mobitems:saddle",  "mcl_core:iron_nugget" },
+                    { "mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget" },
+                }
+            })
+
+            minetest.register_craft({
+                output = "mcl_mobitems:gold_horse_armor",
+                recipe = {
+                    { "mcl_core:gold_nugget", "mcl_core:gold_nugget", "mcl_core:gold_nugget" },
+                    { "mcl_core:gold_nugget", "mcl_mobitems:saddle",  "mcl_core:gold_nugget" },
+                    { "mcl_core:gold_nugget", "mcl_core:gold_nugget", "mcl_core:gold_nugget" },
+                }
+            })
+
+            minetest.register_craft({
+                output = "mcl_mobitems:diamond_horse_armor",
+                recipe = {
+                    { "mcl_core:diamond", "mcl_core:diamond",    "mcl_core:diamond" },
+                    { "mcl_core:diamond", "mcl_mobitems:saddle", "mcl_core:diamond" },
+                    { "mcl_core:diamond", "mcl_core:diamond",    "mcl_core:diamond" },
+                }
+            })
+        end
+    end
+
+    -- heads
+    -- disable by default, those are rare enough.
+    if
+        minetest.get_modpath("mcl_heads")
+        and minetest.settings:get_bool("mcl_misk_recipes.enable_heads", false)
+    then
+        minetest.register_craft({
+            output = "mcl_heads:zombie",
+            recipe = {
+                { "mcl_mobitems:rotten_flesh", "mcl_mobitems:rotten_flesh", "mcl_mobitems:rotten_flesh" },
+                { "mcl_mobitems:spider_eye",   "",                          "mcl_mobitems:spider_eye" },
+            }
+        })
+        -- minetest.register_craft({
+        --     output = "mcl_heads:creeper",
+        --     recipe = {
+        --         { "mcl_mobitems:gunpowder", "mcl_mobitems:gunpowder", "mcl_mobitems:gunpowder" },
+        --         { "mcl_mobitems:gunpowder", "",                       "mcl_mobitems:gunpowder" },
+        --     }
+        -- })
+        minetest.register_craft({
+            output = "mcl_heads:skeleton",
+            recipe = {
+                { "mcl_mobitems:bone", "mcl_mobitems:bone", "mcl_mobitems:bone" },
+                { "mcl_mobitems:bone", "",                  "mcl_mobitems:bone" },
+            }
+        })
+    end
+
+    if
+        minetest.get_modpath("mcl_jukebox")
+        and minetest.settings:get_bool("mcl_misk_recipes.enable_jukebox_records", true)
+    then
+        -- to have different recipes for each record.
+
+        local jukeboxRadomizer = {}
+
+        for craftitem, _ in pairs(minetest.registered_craftitems) do
+            if
+            -- @todo it ain't much. and it's not very honest work either
+            -- gives us plenty to work with though. but could still run out.
+                (
+                -- why no regular regular expressions lua?
+                    string.find(craftitem, 'mcl_core')
+                    or string.find(craftitem, 'mcl_dyes?')
+                    or string.find(craftitem, 'mcl_farming')
+                )
+                and not string.find(craftitem, "enchanted")
+            then
+                table.insert(jukeboxRadomizer, craftitem)
+            end
+        end
+        print(dump(jukeboxRadomizer))
+        local i = 1
+        for key, _ in pairs(mcl_jukebox.registered_records) do
+            local randCraftItem = jukeboxRadomizer[i] or ""
+            i = i + 1
+            minetest.register_craft({
+                output = key,
+                recipe = {
+                    { randCraftItem,        "mcl_core:coal_lump", "mesecons:redstone" },
+                    { "mcl_core:coal_lump", "mcl_core:paper",     "mcl_core:coal_lump" },
+                    { "mesecons:redstone",  "mcl_core:coal_lump", randCraftItem },
+                }
+            })
+        end
+    end
+end)
---
+++ readme.md
@@ -0,0 +1,42 @@
+Unsolicited Crafting Reipes for Mineclonia
+==========================================
+
+This is a companion mod for [k_recyclebin](https://content.minetest.net/packages/ketwaroo/k_recyclebin/)
+
+And no it's not a typo. `misk` instead of `misc` to keep up the `k` nameing theme of my mods and to avoid collisions in case someone else comes up with an actually useful list of miscellaneous recipes in the future.
+
+### But, Why?
+
+In `mineclonia`, and other minecraft clones, there are many items that can be found in the wild but cannot be crafted.
+
+For example, saddles and various horse armors.
+
+If you are using the recycle bin in `k_recyclebin`, it sets a damper on things to discover that you cannot break down those items and have to resort to having a lava pool in your back yard, use them as really inadequate fuel sources, or even toss em down a chasm somewhere.
+
+It is a limited use mod but it adds the options to get some useful materials out of these misc items.
+
+### Recipes included
+
+These are best quess recipes. I've taken some liberties here.
+
+ * Saddle `mcl_mobitems:saddle`
+ * ~~leather horse armor~~ doesn't exist in mineclonia right now. It should though. Will investigate later if bored.
+ * Iron Horse Armor `mcl_mobitems:iron_horse_armor`
+ * Golden Horse Armor `mcl_mobitems:gold_horse_armor`
+ * Diamond Horse Armor `mcl_mobitems:diamond_horse_armor`
+ * Various records `mcl_jukebox:record_*` (see notes below)
+ * A few rare heads (disabled by default)
+
+Check settings for this mod to enable/disable a few things.
+
+## IMPORTANT
+
+### Mineclone 2 not supported.
+
+Mineclone 2/5 not supported. I don't have those installed locally, at least for now. I prefer the cherry blossom biomes of Mineclonia way more, and so should you. Many of the item names are shared between those minecraft clones so it may work to some extent. Supply a pull request if something is broken on your end in Mineclone 2.
+
+### Jukebox records (and other randomness)
+
+Some items like vynil records never had any business being craftable. To be able to differentiate between each record, some random craft item is used in each recipe. Because I'm lazy and there's nothing you can do to stop me, the randomness is programmatic and determined at load time. So each time you create a server, the recipe for each record can be different from last time.
+
+Currently limited to music records but there may be other non craftables that will use the same strategy.
